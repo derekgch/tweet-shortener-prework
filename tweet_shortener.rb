f5715330dcccc694ad35b8e8d1a9f3d12e40d88f
derek chen <derek.g.chen@gmail.com>
2018-03-24 14:17:13 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,12 @@
 def dictionary
+ words_list = {
+  {"hello" => 'hi'},
+  {"to, two, too" => '2' },
+  {"for, four" => '4'},
+  {'be' => 'b'},
+  {'you' => 'u'},
+  {"at" => "@" },
+  {"and" => "&"}
 
 end
 
