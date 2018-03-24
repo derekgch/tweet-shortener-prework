a127ca688f77ac7188a7b455329e7ab49e75b02e
derek chen <derek.g.chen@gmail.com>
2018-03-24 14:41:33 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -25,7 +25,7 @@ def word_substituter (tweet="some thing need to be shorten, like you too")
   temp = ""
   temp_a.each do |word|
     if words_can_b_sh.include?(word.downcase)
-      temp << dictionary[word]
+      temp << dictionary[word]+ " "
     else
       temp << word+ " "
     end
