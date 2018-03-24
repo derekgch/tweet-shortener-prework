37b58de8fcb2bba79964e8a9a788b14bc32c4b75
derek chen <derek.g.chen@gmail.com>
2018-03-24 15:13:26 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,8 +40,9 @@ end
 def bulk_tweet_shortener (tweets)
   if(tweets.size > 0)
     tweets.each do |tweet_each|
+      puts word_substituter(tweet_each)
 
-      puts tweet_each
+      # puts tweet_each
       # if(!tweet_each.empty?)
       #   puts word_substituter(tweet_each)
       # end
