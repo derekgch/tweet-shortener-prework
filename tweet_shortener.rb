55b88432c0a3b8024b1c225311bf7df61df4336e
derek chen <derek.g.chen@gmail.com>
2018-03-24 15:19:04 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -35,7 +35,7 @@ end
 
 tw="I'm running out of example tweets for you guys, "
 tw2="which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real."
-puts word_substituter(tw)
+puts word_substituter(tw2)
 
 #  #bulk_tweet_shortener
 #    shortens each tweet and prints the results (FAILED - 2)
